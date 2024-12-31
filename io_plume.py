8c4e3c489381ed08da8875802dfb7663820a7260
Nicholas Stroupe <data.nik.tech@gmail.com>
2024-12-31 14:05:23 -0600

    Add Comments

--- io_plume.py
+++ io_plume.py
@@ -1,3 +1,4 @@
+# Importing modules, initializing pygame, and defining colors.
 import sys
 import math
 import random
@@ -11,16 +12,16 @@ LT_GRAY = (180, 180, 180)
 GRAY = (120, 120, 120)
 DK_GRAY = (80, 80, 80)
 
-
+# Defining the particle class.
 class Particle(pg.sprite.Sprite):
     gases_colors = {'SO2': LT_GRAY, 'CO2': GRAY, 'H2S': DK_GRAY, 'H2O': WHITE}
 
     VENT_LOCATION_XY = (320, 300)
     IO_SURFACE_Y = 308
-    GRAVITY = 0.5                # pixels_per_frame; added to dy each game loop
-    VELOCITY_SO2 = 8             # pixels_per_frame
+    GRAVITY = 0.5                # pixels_per_frame; added to dy each game loop.
+    VELOCITY_SO2 = 8             # pixels_per_frame.
 
-    # scalars (SO2 atomic weight/ particle atomic weight) used for velocity
+    # scalars (SO2 atomic weight/ particle atomic weight) used for velocity.
     vel_scalar = {'SO2': 1, 'CO2': 1.45, 'H2S': 1.9, 'H2O': 3.6}
 
     def __init__(self, screen, background, *groups):
@@ -34,13 +35,13 @@ class Particle(pg.sprite.Sprite):
         self.vel = Particle.VELOCITY_SO2 * Particle.vel_scalar[self.gas]
         self.x, self.y = Particle.VENT_LOCATION_XY
         self.vector()
-
+# Ejecting tha particle.
     def vector(self):
         orient = random.uniform(60, 120)
         radians = math.radians(orient)
         self.dx = self.vel * math.cos(radians)
         self.dy = -self.vel * math.sin(radians)
-
+# Updataing the particle and handling boundary conditions.
     def update(self):
         """Apply gravity, draw path, and handle boundary conditions"""
         self.dy += Particle.GRAVITY
@@ -55,14 +56,14 @@ class Particle(pg.sprite.Sprite):
         if self.y < 0 or self.y > Particle.IO_SURFACE_Y:
             self.kill()
 
-
+# Defining the main function.
 def main():
     """Set up and run game screen and loop"""
     screen = pg.display.set_mode((639, 360))
     pg.display.set_caption('Io Volcano Simulator')
     background = pg.image.load("jupytermoon.jpg")
 
-    # set up color coded legend
+    # set up color coded legend.
     legend_font = pg.font.SysFont('None', 24)
     water_label = legend_font.render('--- H2O', True, WHITE, BLACK)
     h2s_label = legend_font.render('--- H2S', True, DK_GRAY, BLACK)
@@ -72,7 +73,7 @@ def main():
     particles = pg.sprite.Group()
 
     clock = pg.time.Clock()
-
+# Completing the main function.
     while True:
         clock.tick(25)
         particles.add(Particle(screen, background, particles))
@@ -92,6 +93,6 @@ def main():
 
         pg.display.flip()
 
-
+# Run the simulation! :)
 if __name__ == "__main__":
     main()
